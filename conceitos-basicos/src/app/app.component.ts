64d8cb4e92f6932f54fb1c2d287848404ad32457
julio <juliocesar.gm6000@gmail.com>
2026-08-17 17:55:52 -0300

    atualizando conceitos basicos

---
+++ conceitos-basicos/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [CalculadoraComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'conceitos-basicos';
+}
---
+++ conceitos-basicos/src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [ FormsModule ],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.scss'
+})
+export class CalculadoraComponent {
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado(){
+    console.log("Chamando o metodo para calcular o resultado!")
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+}
