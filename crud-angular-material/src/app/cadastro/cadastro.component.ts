ca105bba3b9fa76401b8961c8b39d5577a56da79
julio <juliocesar.gm6000@gmail.com>
2026-08-18 17:48:35 -0300

    parte visual do formulario

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { matCardModule } from '@angular/material/card';
+import { Component } from '@angular/core'
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule } from '@angular/forms'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, matCardModule],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
