b221ed918c1fd22914ebad3f99a4c8eae04a5d71
julio <juliocesar.gm6000@gmail.com>
2026-08-20 14:23:45 -0300

    criando formulário de pesquisa

--- udemy-douglas/crud-angular-material/src/app/consulta/consulta.component.ts
+++ udemy-douglas/crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
