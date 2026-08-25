110cff87f36b1ecf1559133b9775e4f2a4da22bb
julio <juliocesar.gm6000@gmail.com>
2026-08-25 13:59:27 -0300

    Adicionando validação no form de lugares

--- udemy-douglas/passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ udemy-douglas/passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Categoria } from '../../categorias/categoria';
 import { CategoriaService } from '../../categorias/categoria.service';
+import { LugarService } from '../lugar.service';
 
 @Component({
   selector: 'app-lugar',
@@ -13,7 +14,10 @@ export class LugarComponent implements OnInit {
   camposForm: FormGroup;
   categorias: Categoria[] = []
 
-  constructor(private categoriasService: CategoriaService){
+  constructor(
+    private categoriasService: CategoriaService,
+    private service: LugarService
+  ){
     this.camposForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       categoria: new FormControl('', Validators.required),
@@ -31,8 +35,26 @@ export class LugarComponent implements OnInit {
   }
 
   salvar(){
-    console.log(this.camposForm.value); 
+
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+        this.service.salvar(this.camposForm.value).subscribe({
+          next: (lugar) => {
+            console.log('Lugar salvo com sucesso', lugar);
+            this.camposForm.reset();
+          },
+          error: (erro) => {
+            console.error('Erro ao salvar o lugar', erro);
+          }
+        });
+    }
+
+
   }
-
-
+        
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return (campo?.invalid && campo?.touched && campo?.errors?.['required']) || false;
+  }
 }
