52b6c9a41a6159a143b2122616039ed9f3c2527f
julio <juliocesar.gm6000@gmail.com>
2026-08-25 11:29:20 -0300

    Criando a estrutura da tela de lugares e adicionando os campos do formulario de lugares

--- udemy-douglas/passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ udemy-douglas/passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
 
 @Component({
   selector: 'app-lugar',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './lugar.component.scss'
 })
 export class LugarComponent {
+  camposForm: FormGroup;
+  categorias: Categoria[] = []
+
+  constructor(){
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+      
+    });
+  }
+
+  salvar(){
+    console.log(this.camposForm.value); 
+  }
+
 
 }
